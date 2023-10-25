a87e0ed912e5fb1c55904099ea525c2dd084ac65
abdusalom <boriboyevabdusalom@gmail.com>
2023-10-25 09:56:07 +0500

    ddd

--- build_func04.py
+++ build_func04.py
@@ -1,11 +1,11 @@
 def main(n):
     """A integer type variable 'n' is given. Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func04 
-
+a = pow((2 + n /3), 2)
     Args:
         n (int): integer
         
     Returns:
         float: the value of the expression
-    """
+    """print(a)
     return
