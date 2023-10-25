e433b5353bd11af9ff5ad7851176068b44586ce3
abdusalom <boriboyevabdusalom@gmail.com>
2023-10-25 09:44:44 +0500

    kkk

--- build_func02.py
+++ build_func02.py
@@ -1,11 +1,11 @@
 def main():
     """Return the value of the expression in README.md file.
     https://github.com/codeschool43/Build_in_function_homework#build_func02 
-
+a = round(3 * (7/5 - 9/4))
     Args:
         None
         
     Returns:
         float: the value of the expression
-    """
+    """print(a)
     return
